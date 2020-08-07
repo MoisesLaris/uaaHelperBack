15551680b163d6b9896210386c61096e6d46d3b3
Moises Laris Santos <3119402@live.com>
2020-08-06 21:09:50 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Exportar
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strinc'
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3800;
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://ProyectoCheco:proyectoch3c0@proyectocheco-v8da3.azure.mongodb.net/uaa?retryWrites=true&w=majority', { useMongoClient: true })
+    //mongoose.connect('mongodb+srv://ProyectoCheco:proyectoch3c0@proyectocheco-v8da3.azure.mongodb.net/congreso?retryWrites=true&w=majority', { useMongoClient: true })
+    //mongoose.connect('mongodb://192.168.0.114:27017/congreso', { mongos: true })
+    .then(() => {
+        console.log('Conexión exitosa! Gracias al moisi precioso');
+
+        //Crear servidor
+        app.listen(port, () => {
+            console.log("Servidor corriendo en localhost:3800");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
