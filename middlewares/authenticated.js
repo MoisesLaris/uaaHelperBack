e9c651ad5405155c23b7a09061399f6d75a79d97
Moises Laris Santos <3119402@live.com>
2020-09-02 17:58:26 -0500

    cambios bd

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -6,6 +6,7 @@ var secret = 'aplicacion_para_la_tesis_de_la_uaa_jeje';
 
 exports.ensureAuth = function(req, res, next) {
     if (!req.headers.authorization) {
+        console.log(req.headers);
         return res.status(403).send({ message: 'La petición no tiene la cabecera de autenticación' });
     }
     var token = req.headers.authorization.replace(/['"]+/g, '');
